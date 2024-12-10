627c5f41d86a96255127c2e2788a93a6153b2aea
Kyle Coffey <me@kylecoffey.com>
2024-12-10 16:57:14 -0600

    day 9 zig

---
+++ day09/Day09.zig
@@ -0,0 +1,130 @@
+const std = @import("std");
+
+const Block = struct {
+    id: ?usize = null,
+    length: u8,
+};
+
+const AllowFragments = enum { allow_fragments, disallow_fragments };
+
+const Fs = std.DoublyLinkedList(Block);
+
+fn relocateBlock(
+    gpa: std.mem.Allocator,
+    fs: *Fs,
+    start: ?*Fs.Node,
+    j: ?*Fs.Node,
+    fragments: AllowFragments,
+) !void {
+    var i = start;
+    while (i != j) : (i = i.?.next) {
+        if (i.?.data.id != null) {
+            // noop
+        } else if (i.?.data.length == j.?.data.length) {
+            std.mem.swap(Block, &i.?.data, &j.?.data);
+            return;
+        } else if (i.?.data.length > j.?.data.length) {
+            const d = i.?.data.length - j.?.data.length;
+            i.?.data = j.?.data;
+            j.?.data.id = null;
+            const node = try gpa.create(Fs.Node);
+            node.* = .{ .data = .{ .length = d } };
+            fs.insertAfter(i.?, node);
+            return;
+        } else if (i.?.data.length < j.?.data.length and fragments == .allow_fragments) {
+            const d = j.?.data.length - i.?.data.length;
+            i.?.data.id = j.?.data.id;
+            j.?.data.length = d;
+            const node = try gpa.create(Fs.Node);
+            node.* = .{ .data = .{ .length = i.?.data.length } };
+            fs.insertAfter(j.?, node);
+        }
+    }
+}
+
+fn compactFs(
+    gpa: std.mem.Allocator,
+    fs: *Fs,
+    fragments: AllowFragments,
+) !void {
+    var i = fs.first;
+    var j = fs.last;
+    while (i != j) {
+        if (i.?.data.id != null) {
+            i = i.?.next;
+        } else if (j.?.data.id == null) {
+            j = j.?.prev;
+        } else {
+            try relocateBlock(gpa, fs, i, j, fragments);
+            j = j.?.prev;
+        }
+    }
+}
+
+fn checksum(fs: Fs) usize {
+    var result: usize = 0;
+    var l: usize = 0;
+    var i = fs.first;
+    while (i) |node| {
+        var k: usize = 0;
+        while (k < node.data.length) : (k += 1) {
+            if (node.data.id) |id| {
+                result += l * id;
+            }
+            l += 1;
+        }
+        i = node.next;
+    }
+    return result;
+}
+
+pub fn main() !void {
+    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa_state.deinit() == .ok);
+    const gpa = gpa_state.allocator();
+
+    const text = try std.io.getStdIn().readToEndAlloc(gpa, std.math.maxInt(usize));
+    defer gpa.free(text);
+    const line = std.mem.trimRight(u8, text, "\r\n");
+    var fs = std.DoublyLinkedList(Block){};
+    defer {
+        var i = fs.first;
+        while (i) |node| {
+            const tmp = node.next;
+            gpa.destroy(node);
+            i = tmp;
+        }
+    }
+    var fs2 = std.DoublyLinkedList(Block){};
+    defer {
+        var i = fs2.first;
+        while (i) |node| {
+            const tmp = node.next;
+            gpa.destroy(node);
+            i = tmp;
+        }
+    }
+    for (line, 0..) |ch, i| {
+        const node = try gpa.create(Fs.Node);
+        node.* = .{
+            .data = .{
+                .id = if (i % 2 == 0) i / 2 else null,
+                .length = ch - '0',
+            },
+        };
+        const node2 = try gpa.create(Fs.Node);
+        node2.* = node.*;
+        fs.append(node);
+        fs2.append(node2);
+    }
+    try compactFs(gpa, &fs, .allow_fragments);
+    try compactFs(gpa, &fs2, .disallow_fragments);
+
+    const part1 = checksum(fs);
+    const part2 = checksum(fs2);
+    try std.io.getStdOut().writer().print(
+        \\Part 1: {d}
+        \\Part 2: {d}
+        \\
+    , .{ part1, part2 });
+}
